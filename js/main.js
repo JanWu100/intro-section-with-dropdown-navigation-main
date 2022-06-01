20e2f73c2defc34fc7471c91d69b28d7630716af
JanWu100 <wagrowskij@gmail.com>
2022-06-01 15:05:33 +0200

    finish challenge

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 document.querySelector(".nav__burger").addEventListener("click", () => {
-  console.log("a");
   document.querySelector(".nav__menu").classList.remove("nav__menu--hidden");
 });
 document
   .querySelector(".nav__menu__close-icon")
   .addEventListener("click", () => {
-    console.log("b");
     document.querySelector(".nav__menu").classList.add("nav__menu--hidden");
   });
 
